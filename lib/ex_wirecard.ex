1ae4c70267e4936c9dcbdd654f2cf0d0aa3a7972
Jonatan Männchen <jonatan@maennchen.ch>
2018-02-26 11:38:13 +0100

    Mix Init

---
+++ lib/ex_wirecard.ex
@@ -0,0 +1,18 @@
+defmodule ExWirecard do
+  @moduledoc """
+  Documentation for ExWirecard.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWirecard.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_wirecard_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWirecardTest do
+  use ExUnit.Case
+  doctest ExWirecard
+
+  test "greets the world" do
+    assert ExWirecard.hello() == :world
+  end
+end
